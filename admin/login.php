ef30f897489a63b3e34bb145874638a14c3447a7
canergulerr <canerguleer@hotmail.com>
2026-02-02 15:44:35 +0300

    Enable debug mode in login

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,9 @@
 <?php
 include dirname(__DIR__) . '/config.php';
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'includes/db.php';
 
 $error = '';
