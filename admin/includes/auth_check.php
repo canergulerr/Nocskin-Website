d04c7840386ca3a2cd23b2958c992d011eb804a6
canergulerr <canerguleer@hotmail.com>
2026-02-02 17:40:46 +0300

    Fix whitespace in auth_check.php

--- admin/includes/auth_check.php
+++ admin/includes/auth_check.php
@@ -1,9 +1,8 @@
-﻿<?php include dirname(__DIR__, 2) . '/config.php'; ?>
-<?php
+﻿<?php
+include dirname(__DIR__, 2) . '/config.php';
 session_start();
 
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
     header("Location: login.php");
     exit;
 }
-?>
